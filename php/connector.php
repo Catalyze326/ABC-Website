7990c9a318be5a6747aaf1b68ea7d0408929dd56
Catalyze326 <cfmcool@gmail.com>
2019-09-05 23:10:07 -0400

    Put all in one folder

--- php/connector.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "grandpa", "hyGnB7NJ5F%WQF6fTSOyy6btQogPYy5KUkYNY#9eBkBqjK&@@ABbU!LE@dtiv", "grandpaSite");
-
-if($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
-}
--- php/secret/mysql.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "loginServer", "rZZHZ&h&RESig@LGKvx^H1@8W9onNQMURzZqKRg%u", "somewhatAdequateUsers");
-if($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
-}
